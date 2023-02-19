a683b4618a6f17a168bb9debd482fea7c8197640
CamHaoHa <camhaovn@gmail.com>
2023-02-19 23:13:28 +1100

    build: path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Demo from '@components/Demo';
 
 const App = () => {
   return (
     <View>
       <Text>App</Text>
+      <Demo />
     </View>
   );
 };
